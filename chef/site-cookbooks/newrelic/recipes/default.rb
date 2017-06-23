fd692b4d0e61a4092e913bf22c09b003d2242a09
Yuriy Novikov <jv.novikov@gmail.com>
2017-06-23 16:07:43 +0000

    upd

--- chef/site-cookbooks/newrelic/recipes/default.rb
+++ chef/site-cookbooks/newrelic/recipes/default.rb
@@ -1,1 +1,21 @@
-default.rb
+
+execute 'get newrelic rpm' do
+  user 'root'
+  command 'rpm -Uvh https://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm'
+end
+
+yum_package 'newrelic-sysmond'
+
+
+execute 'register license' do
+  user 'root'
+  command 'nrsysmond-config --set license_key=69ce0ffeb1ab7e55ec8a988c8d050f513a3d0ffb'
+end
+
+service 'newrelic-sysmond' do
+  action :enable
+end
+
+service 'newrelic-sysmond' do
+  action :start
+end
